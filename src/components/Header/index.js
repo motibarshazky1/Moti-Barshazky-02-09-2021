84e92bb510215cb3a768a6ea84340ac55ca2ecc4
Moti <motib@rightcodelabs.com>
2021-09-02 16:49:12 +0300

    create header component for app

---
+++ src/components/Header/index.js
@@ -0,0 +1,78 @@
+import { AppBar, Toolbar, Typography, makeStyles, Button } from '@material-ui/core';
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import './index.css';
+
+const headersData = [
+	{
+		label: 'Home',
+		href: '/',
+	},
+	{
+		label: 'Favorites',
+		href: '/favorites',
+	},
+];
+
+const useStyles = makeStyles(() => ({
+	appBar: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	menuWrapper: {
+		display: 'flex',
+	},
+	menuLeft: {
+		display: 'flex',
+		paddingLeft: '50px',
+		fontWeight: 700,
+	},
+	menuButtons: {
+		display: 'flex',
+		paddingRight: '30px',
+	},
+	menuButton: {
+		display: 'flex',
+		fontSize: '15px',
+		padding: '20px',
+		fontFamily: 'Montserrat, Roboto, OpenSans',
+	},
+}));
+
+function Header() {
+	const { menuButtons, menuButton, menuLeft, appBar } = useStyles();
+
+	const webTitle = () => {
+		return <Toolbar className={menuLeft}>Herolo Weather Task</Toolbar>;
+	};
+
+	const getMenuButtons = () => {
+		return headersData.map(({ label, href }) => {
+			return (
+				<Button
+					{...{
+						key: label,
+						color: 'inherit',
+						to: href,
+						component: RouterLink,
+						className: menuButton,
+					}}
+				>
+					{label}
+				</Button>
+			);
+		});
+	};
+
+	return (
+		<header>
+			<AppBar className={appBar}>
+				{webTitle()}
+				<div className={menuButtons}>{getMenuButtons()}</div>
+			</AppBar>
+		</header>
+	);
+}
+
+export default Header;
