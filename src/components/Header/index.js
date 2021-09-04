a9c7ea5645d724208faefcd720cfda154223c630
Moti <motib@rightcodelabs.com>
2021-09-04 16:57:41 +0300

    fix design of degress toggle

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import { Link as RouterLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import { AppBar, Toolbar, makeStyles, Button } from '@material-ui/core';
+import Switch from 'react-switch';
 
 import { toggleDegreeUnits } from '../../actions/environmentActions';
 
@@ -14,6 +15,7 @@ const useStyles = makeStyles(() => ({
 		display: 'flex',
 		flexDirection: 'row',
 		justifyContent: 'space-between',
+		alignItems: 'center',
 	},
 	menuWrapper: {
 		display: 'flex',
@@ -55,6 +57,7 @@ const Header = () => {
 	const location = useLocation();
 	const { menuButtons, menuButton, menuButtonSelected, webTitle, appBar, svg } = useStyles();
 	const [selectedHeader, setSelectedHeader] = useState('');
+	const [isChecked, setIsChecked] = useState(false);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -67,17 +70,36 @@ const Header = () => {
 	}, [location]);
 
 	const onClickToggle = () => {
+		setIsChecked(!isChecked);
 		dispatch(toggleDegreeUnits());
 	};
 
 	return (
 		<div className="header-wrapper">
 			<AppBar className={appBar}>
-				<Toolbar className={webTitle}>Herolo Weather Task</Toolbar>{' '}
+				<Toolbar className={webTitle}>Herolo Weather Task</Toolbar>
+				<div className="example">
+					<Switch
+						onChange={onClickToggle}
+						width={55}
+						checked={isChecked}
+						uncheckedIcon={
+							<div className="check">
+								<svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24">
+									<path d="M6 7c.551 0 1 .449 1 1s-.449 1-1 1c-.551 0-1-.449-1-1s.449-1 1-1zm0-1c-1.105 0-2 .895-2 2s.895 2 2 2 2-.895 2-2-.895-2-2-2zm11.984 7.949c-.091 1.257-.555 2.246-1.392 2.968-.836.722-1.939 1.083-3.309 1.083-1.497 0-2.675-.504-3.533-1.512-.858-1.008-1.287-2.392-1.287-4.151v-.714c0-1.123.198-2.112.593-2.968.396-.855.961-1.512 1.697-1.969.735-.457 1.589-.686 2.562-.686 1.348 0 2.433.361 3.257 1.083.824.722 1.299 1.735 1.428 3.04h-2.406c-.059-.754-.269-1.301-.63-1.64-.361-.34-.91-.509-1.648-.509-.802 0-1.402.288-1.801.862-.398.575-.603 1.467-.614 2.675v.882c0 1.262.191 2.185.573 2.767.382.583.985.874 1.809.874.743 0 1.298-.17 1.664-.509.366-.34.576-.865.63-1.576h2.407zm-5.984-11.949c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z" />
+								</svg>
+							</div>
+						}
+						checkedIcon={
+							<div className="check">
+								<svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24">
+									<path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-5 7c.551 0 1 .449 1 1s-.449 1-1 1-1-.449-1-1 .449-1 1-1zm0-1c-1.105 0-2 .895-2 2s.895 2 2 2 2-.895 2-2-.895-2-2-2zm9.857 7.096h-4.385v4.904h-2.472v-12h7.352v2.003h-4.879v3.099h4.385v1.994z" />
+								</svg>
+							</div>
+						}
+					/>
+				</div>
 				<div className={menuButtons}>
-					<Button onClick={onClickToggle} style={{ padding: '20px 35px 20px 10px', marginRight: '40px' }}>
-						TOGGLE
-					</Button>
 					<svg
 						onClick={() => setSelectedHeader('home')}
 						className={svg}
