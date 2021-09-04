25eb1d156ee0ed9fac7ecce35a21a6f236e4f02e
Moti <motib@rightcodelabs.com>
2021-09-04 13:50:35 +0300

    change header's home icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -23,11 +23,12 @@ const useStyles = makeStyles(() => ({
 	menuButtons: {
 		display: 'flex',
 		paddingRight: '30px',
+		justifyContent: 'center',
+		alignItems: 'center',
 	},
 	menuButton: {
 		display: 'flex',
 		fontSize: '17px',
-		padding: '20px',
 		fontFamily: 'Montserrat, Roboto, OpenSans',
 		transition: 'all 0.5s linear',
 		color: 'darkgrey',
@@ -35,15 +36,18 @@ const useStyles = makeStyles(() => ({
 	menuButtonSelected: {
 		display: 'flex',
 		fontSize: '17px',
-		padding: '20px',
 		color: 'white',
 		fontFamily: 'Montserrat, Roboto, OpenSans',
 		transition: 'all 0.5s linear',
 	},
+	svg: {
+		transition: 'all 0.5s linear',
+		cursor: 'pointer',
+	},
 }));
 
 const Header = () => {
-	const { menuButtons, menuButton, menuButtonSelected, webTitle, appBar } = useStyles();
+	const { menuButtons, menuButton, menuButtonSelected, webTitle, appBar, svg } = useStyles();
 	const [selectedHeader, setSelectedHeader] = useState('');
 
 	useEffect(() => {
@@ -64,8 +68,21 @@ const Header = () => {
 			<AppBar className={appBar}>
 				{getWebTitle()}
 				<div className={menuButtons}>
-					<Button
+					<svg
 						onClick={() => setSelectedHeader('home')}
+						className={svg}
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 26 26"
+						fill={`${selectedHeader === 'home' ? 'white' : 'darkgrey'}`}
+					>
+						<path d="M12 2h2v2h2v3.702l7 2.618v12.68h1v1h-24v-1h1v-11h6v-8h2v-2h2v-2h1v2zm3 3h-7v18h1v-2h5v2h1v-18zm-2 17h-3v1h3v-1zm8 1h1v-11.987l-6-2.243v14.23h1v-2h4v2zm-14-10h-5v10h1v-2h3v2h1v-10zm-2 9h-1v1h1v-1zm15 0h-2v1h2v-1zm-16-5v2h-1v-2h1zm2 0v2h-1v-2h1zm5-10v12h-1v-12h1zm10 11v1h-4v-1h4zm-8-11v12h-1v-12h1zm8 9v1h-4v-1h4zm-17-2v2h-1v-2h1zm2 0v2h-1v-2h1zm15 0v1h-4v-1h4zm0-2v1h-4v-1h4zm-8-9h-3v1h3v-1z" />{' '}
+					</svg>
+					<Button
+						disableFocusRipple={true}
+						onClick={() => setSelectedHeader('home')}
+						style={{ padding: '20px 35px 20px 10px' }}
 						{...{
 							key: 'Home',
 							color: 'inherit',
@@ -76,8 +93,22 @@ const Header = () => {
 					>
 						Home
 					</Button>
+					<svg
+						onClick={() => setSelectedHeader('favorites')}
+						className={svg}
+						width="24"
+						height="24"
+						viewBox="0 0 26 26"
+						xmlns="http://www.w3.org/2000/svg"
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						fill={`${selectedHeader === 'favorites' ? 'white' : 'darkgrey'}`}
+					>
+						<path d="M15.668 8.626l8.332 1.159-6.065 5.874 1.48 8.341-7.416-3.997-7.416 3.997 1.481-8.341-6.064-5.874 8.331-1.159 3.668-7.626 3.669 7.626zm-6.67.925l-6.818.948 4.963 4.807-1.212 6.825 6.068-3.271 6.069 3.271-1.212-6.826 4.964-4.806-6.819-.948-3.002-6.241-3.001 6.241z" />
+					</svg>
 					<Button
 						onClick={() => setSelectedHeader('favorites')}
+						style={{ padding: '20px 20px 20px 10px' }}
 						{...{
 							key: 'Favorites',
 							color: 'inherit',
