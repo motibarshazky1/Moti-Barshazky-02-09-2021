e26bb70b859aa517742ed15c493d81d504fcaac3
Moti <motib@rightcodelabs.com>
2021-09-06 12:14:38 +0300

    add animation to favorite city page

--- src/components/FavoriteCity/index.js
+++ src/components/FavoriteCity/index.js
@@ -1,24 +1,36 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import './index.css';
 
+const item = {
+	display: 'flex',
+	hidden: { y: 20, opacity: 0 },
+	visible: {
+		y: 0,
+		opacity: 1,
+	},
+};
+
 const FavoriteCity = ({ cityName, cityKey, currentDegrees, currentTitle, onClickFavoriteCity, units }) => {
 	return (
-		<div className="city-wrapper" key={cityKey} onClick={() => onClickFavoriteCity(cityKey, cityName)}>
-			<svg
-				fill="yellow"
-				className="fav-icon"
-				xmlns="http://www.w3.org/2000/svg"
-				width="32"
-				height="32"
-				viewBox="0 0 24 24"
-			>
-				<path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-			</svg>
-			<label className="label name">{cityName}</label>
-			<label className="label degrees">{`${currentDegrees}° ${units}`}</label>
-			<label className="label title">{currentTitle}</label>
-		</div>
+		<motion.li key={cityKey} className="item" variants={item}>
+			<div className="city-wrapper" key={cityKey} onClick={() => onClickFavoriteCity(cityKey, cityName)}>
+				<svg
+					fill="yellow"
+					className="fav-icon"
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+				>
+					<path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+				</svg>
+				<label className="label name">{cityName}</label>
+				<label className="label degrees">{`${currentDegrees}° ${units}`}</label>
+				<label className="label title">{currentTitle}</label>
+			</div>
+		</motion.li>
 	);
 };
 
--- src/pages/FavoritesPage/index.js
+++ src/pages/FavoritesPage/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+import { motion } from 'framer-motion';
+
 import FavoriteCity from '../../components/FavoriteCity';
 
 import './index.css';
+
+const container = {
+	display: 'flex',
+	hidden: { opacity: 1, scale: 0 },
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			delayChildren: 0.3,
+			staggerChildren: 0.2,
+		},
+	},
+};
 
 const FavoritesPage = () => {
 	const history = useHistory();
@@ -18,17 +33,32 @@ const FavoritesPage = () => {
 
 	return (
 		<div className="favorites-wrapper">
-			{cities.map((city) => (
-				<FavoriteCity
-					key={city.id}
-					cityKey={city.id}
-					cityName={city.name}
-					currentDegrees={units === 'c' ? city.currentCityWeather.degreesC : city.currentCityWeather.degreesF}
-					currentTitle={city.currentCityWeather.title}
-					onClickFavoriteCity={onClickFavoriteCity}
-					units={units}
-				/>
-			))}
+			{/* {cities.map((city) => (
+					<FavoriteCity
+						key={city.id}
+						cityKey={city.id}
+						cityName={city.name}
+						currentDegrees={units === 'c' ? city.currentCityWeather.degreesC : city.currentCityWeather.degreesF}
+						currentTitle={city.currentCityWeather.title}
+						onClickFavoriteCity={onClickFavoriteCity}
+						units={units}
+					/>
+				))} */}
+			{cities && (
+				<motion.ul className="container" variants={container} initial="hidden" animate="visible">
+					{cities.map((city) => (
+						<FavoriteCity
+							key={city.id}
+							cityKey={city.id}
+							cityName={city.name}
+							currentDegrees={units === 'c' ? city.currentCityWeather.degreesC : city.currentCityWeather.degreesF}
+							currentTitle={city.currentCityWeather.title}
+							onClickFavoriteCity={onClickFavoriteCity}
+							units={units}
+						/>
+					))}
+				</motion.ul>
+			)}
 		</div>
 	);
 };
